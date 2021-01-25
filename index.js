5120b6e8643e8b058f9928df5b0160997ba2d779
Ayanna Aziz <ayanna.aziz77@gmail.com>
2021-01-25 13:02:38 -0500

    added ds store

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-// Internal NPMs
+// 
 const api = require('./utils/api.js');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
