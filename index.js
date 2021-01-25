4ecdd8b34659df1a243f04aa70eafc03c87bd030
Ayanna Aziz <ayanna.aziz77@gmail.com>
2021-01-25 13:42:05 -0500

    get user response and properly displayed data in readme file

--- index.js
+++ index.js
@@ -20,42 +20,42 @@ function getUserInput () {
         message: 'What is your Github user name?',
         name: 'githubusername',
       },
-      {
-        type: 'input',
-        message: 'What is your email?',
-        name: 'email',
-      },
+      // {
+      //   type: 'input',
+      //   message: 'What is your email?',
+      //   name: 'email',
+      // },
       
-      {
-        type: 'input',
-        message: 'Enter the title of your project.',
-        name: 'title',
-      },
-      {
-        type: 'input',
-        message: 'Enter the description of your project.',
-        name: 'description',
-      },
-      {
-        type: 'input',
-        message: 'Enter the installation instructions  of your project.',
-        name: 'installation',
-      },
-      {
-        type: 'input',
-        message: 'Enter the usage information of your project.',
-        name: 'usage',
-      },
-      {
-        type: 'input',
-        message: 'Enter the contribution guidlines of your project.',
-        name: 'contribution',
-      },
-      {
-        type: 'input',
-        message: 'Enter the test instructions for your project.',
-        name: 'test',
-      },
+      // {
+      //   type: 'input',
+      //   message: 'Enter the title of your project.',
+      //   name: 'title',
+      // },
+      // {
+      //   type: 'input',
+      //   message: 'Enter the description of your project.',
+      //   name: 'description',
+      // },
+      // {
+      //   type: 'input',
+      //   message: 'Enter the installation instructions  of your project.',
+      //   name: 'installation',
+      // },
+      // {
+      //   type: 'input',
+      //   message: 'Enter the usage information of your project.',
+      //   name: 'usage',
+      // },
+      // {
+      //   type: 'input',
+      //   message: 'Enter the contribution guidlines of your project.',
+      //   name: 'contribution',
+      // },
+      // {
+      //   type: 'input',
+      //   message: 'Enter the test instructions for your project.',
+      //   name: 'test',
+      // },
       {
         type: 'list',
         message: 'Choose the type of license.',
@@ -64,16 +64,24 @@ function getUserInput () {
       },
 
     ])
-    .then((response) =>
-      response.confirm === response.password
-        ? console.log('Success!')
-        : console.log('You forgot your password already?!')
+    .then((response) => {
+       // response.confirm === response.password
+       const data = response.githubusername + "\n" + response.license 
+      
+       writeToFile('./utils/ReadMe.md', data)
+       //  console.log('Success!')
+       // : console.log('You forgot your password already?!')
+    }
+     
     );
 }
 
-
 // function to write README file
 function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+ err ? console.error(err) : console.log('Success!')
+ );
+
 }
 
 // function to initialize program
---
+++ utils/ReadMe.md
@@ -0,0 +1,2 @@
+ayanna
+Apache
