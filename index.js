c95cd4301891608ad8aaaf2d039aa0ae2d740874
Ayanna Aziz <ayanna.aziz77@gmail.com>
2021-01-25 13:23:26 -0500

    added questions for user inside function

--- index.js
+++ index.js
@@ -11,12 +11,74 @@ const questions = [
 
 ];
 
+// const inquirer = require('inquirer');
+function getUserInput () {
+    inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: 'What is your Github user name?',
+        name: 'githubusername',
+      },
+      {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+      },
+      
+      {
+        type: 'input',
+        message: 'Enter the title of your project.',
+        name: 'title',
+      },
+      {
+        type: 'input',
+        message: 'Enter the description of your project.',
+        name: 'description',
+      },
+      {
+        type: 'input',
+        message: 'Enter the installation instructions  of your project.',
+        name: 'installation',
+      },
+      {
+        type: 'input',
+        message: 'Enter the usage information of your project.',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'Enter the contribution guidlines of your project.',
+        name: 'contribution',
+      },
+      {
+        type: 'input',
+        message: 'Enter the test instructions for your project.',
+        name: 'test',
+      },
+      {
+        type: 'list',
+        message: 'Choose the type of license.',
+        name: 'license',
+        choices: ["Apache", "Boost", "MIT"]
+      },
+
+    ])
+    .then((response) =>
+      response.confirm === response.password
+        ? console.log('Success!')
+        : console.log('You forgot your password already?!')
+    );
+}
+
+
 // function to write README file
 function writeToFile(fileName, data) {
 }
 
 // function to initialize program
 function init() {
+  getUserInput()
 
 }
 
